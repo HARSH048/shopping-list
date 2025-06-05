6532ffdeaff5ac0a65d4958d523ff4c729611554
Harsh <harshbhardwaj860@gmail.com>
2025-06-06 03:26:22 +0530

    add validation using react-hook-form

--- src/Components/InputItem/InputItem.jsx
+++ src/Components/InputItem/InputItem.jsx
@@ -1,31 +1,49 @@
 import "./InputItem.css";
 import "react-toastify/dist/ReactToastify.css";
-
+import { useForm } from "react-hook-form";
 import { showSucess } from "../../utils/showToast";
-import { useState } from "react";
-export default function InputItem({
-  addItem
-}) {
-  const [itemName, setItemName] = useState("");
+export default function InputItem({ addItem }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+    reset
+  } = useForm();
 
   const handleAddClick = () => {
     showSucess("Successfully Add Item... "); // Display success toast
-    addItem(itemName); // Add the item
-    setItemName(""); // Optionally clear input
+    addItem(getValues("item")); // Add the item
+    reset({
+      data: "item"
+    })
   };
 
   return (
     <div className="input-item-wrapper">
-      <input
-        className="input-item"
-        type="text"
-        placeholder="Add Item..."
-        value={itemName}
-        onChange={(e) => setItemName(e.target.value)}
-      />
-      <button className="add-item-button" onClick={handleAddClick}>
-        Add Item
-      </button>
+      <form onSubmit={handleSubmit(handleAddClick)}>
+        <div className="input-row">
+          <div className="input-with-error">
+            <input
+              className={`input-item ${errors.item ? "error" : ""}`}
+              type="text"
+              placeholder="Add Item..."
+              {...register("item", { required: true, minLength: 3 })}
+            />
+            {errors.item?.type === "required" && (
+              <span className="errorMsg">Item name is required.</span>
+            )}
+            {errors.item?.type === "minLength" && (
+              <span className="errorMsg">
+                Name should be at least 3 characters long.
+              </span>
+            )}
+          </div>
+          <button className="add-item-button" type="submit">
+            Add Item
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
